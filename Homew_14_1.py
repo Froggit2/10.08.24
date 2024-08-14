984cc4db6a435c079e17e74ab9909d678b1dbcba
Froggit2 <165292357+Froggit2@users.noreply.github.com>
2024-08-14 16:49:44 +0300

    Add files via upload

--- Homew_14_1.py
+++ Homew_14_1.py
@@ -1,7 +1,7 @@
 import sqlite3
 
-conection = sqlite3.connect('not_telegram.db')
-cursor = conection.cursor()
+connection = sqlite3.connect('not_telegram.db')  # написание 'connection'
+cursor = connection.cursor()
 
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Users(
@@ -9,8 +9,8 @@ id INTEGER PRIMARY KEY,
 username TEXT NOT NULL,
 email TEXT NOT NULL,
 age INTEGER,
-balance INTAGER NOT NULL
-)''')
+balance INTEGER NOT NULL    
+)''')  # Исправлено 'INTAGER' на 'INTEGER'
 
 ag = 0
 for i in range(10):
@@ -18,9 +18,13 @@ for i in range(10):
     i += 1
     ag += 10
     cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
-                   (f"User{i}", f"example{i}@gmail.com", f"{ag}", f"{bal}"))
-    cursor.execute("UPDATE Users SET balance = ? WHERE age = ?", (500, f"{i // 2 == 0}"))
+                   (f"User{i}", f"example{i}@gmail.com", ag, bal))  # Убраны лишние кавычки для ag и bal
 
+    if i % 2 == 0:  # Обновляем баланс только для четных id
+        cursor.execute("UPDATE Users SET balance = ? WHERE id = ?", (500, i))
 
-conection.commit()
-conection.close()
+    if i % 3 == 0:
+        cursor.execute("DELETE FROM User WHERE username = ?", ("User1", ))
+
+connection.commit()
+connection.close()
